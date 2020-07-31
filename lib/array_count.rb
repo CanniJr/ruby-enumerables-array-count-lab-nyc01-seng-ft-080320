65a6af4ca8a519993a41a98fda6b8646f62a2831
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 02:38:04 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,5 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  array.count do |index|
-    index.even?
 end
 
 def count_empty_strings(array)
